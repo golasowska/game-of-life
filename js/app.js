24daacdde0a4c62fefed39d267aab67fb796655d
Anna Golasowska <annagolasowska@gmail.com>
2018-01-04 20:51:47 +0100

    added new method - computeNextGeneration and corrected createNeighbors method

--- js/app.js
+++ js/app.js
@@ -60,44 +60,53 @@ document.addEventListener('DOMContentLoaded', function (){
     if (x===0 && y === 0) {
      neighbors = [this.position(x+1, y),
      this.position(x, y+1), this.position(x+1, y+1)];
-   } else if (x=== this.width-1 && y===0) {
+   }
+   else if (x=== this.width-1 && y===0) {
      neighbors = [this.position(x-1, y), this.position(x-1, y+1),
       this.position(x, y+1),];
-   } else if (x===this.width-1 && y === this.height-1) {
+   }
+   else if (x===this.width-1 && y === this.height-1) {
      neighbors = [this.position(x-1, y-1), this.position(x, y-1),
     this.position(x-1, y)];
-  } else if (x === 0 && y === this.height-1) {
+  }
+  else if (x === 0 && y === this.height-1) {
      neighbors = [this.position(x, y-1),
     this.position(x+1, y-1),this.position(x+1, y),];
-  } else if (y===0 && x >0 || y===0 && x < this.width) {
+  }
+  else if ((y===0 && x >0) && (y===0 && x < this.width)) {
      neighbors = [this.position(x-1, y),
      this.position(x+1, y), this.position(x-1, y+1),
      this.position(x, y+1), this.position(x+1, y+1)];
-  } else if (x===0 && y > 0 || x===0 && y< this.height) {
+  }
+  else if ((x===0 && y > 0) && (x===0 && y< this.height)) {
      neighbors = [ this.position(x, y-1),this.position(x+1, y-1),
       this.position(x+1, y),this.position(x, y+1),
       this.position(x+1, y+1)];
-  } else if (y === this.height -1 && x>0 || y=== this.height-1 && x < this.width) {
+  }
+  else if ((y === this.height -1 && x>0) && (y=== this.height-1 && x < this.width)) {
      neighbors = [this.position(x-1, y-1), this.position(x, y-1),
     this.position(x+1, y-1), this.position(x-1, y),
     this.position(x+1, y)];
-  } else if (x===0 && y>0 || x===0 && y<this.height) {
-    neighbors = [this.position(x, y-1),
-   this.position(x+1, y-1), this.position(x+1, y),
-   this.position(x, y+1), this.position(x+1, y+1)];
- } else {
+  }
+  else if ((x===this.width-1 && y>0) && (x===this.width-1 && y<this.height-1)) {
+    neighbors = [this.position(x-1, y-1), this.position(x, y-1),
+   this.position(x-1, y),this.position(x-1, y+1),
+   this.position(x, y+1)];
+ }
+ else {
    neighbors = [this.position(x-1, y-1), this.position(x, y-1),
   this.position(x+1, y-1), this.position(x-1, y),
   this.position(x+1, y), this.position(x-1, y+1),
   this.position(x, y+1), this.position(x+1, y+1)];
   }
   return neighbors;
- }
+};
 
 
   GameOfLife.prototype.computeCellNextState = function(x,y){
     var liveNeighbor = [];
     var neighbors = this.createNeighbors(x,y);
+    // console.log('sasiedzi ', neighbors);
 
     neighbors.forEach(function(elem){
       if(elem.classList.contains('live')) {
@@ -105,44 +114,48 @@ document.addEventListener('DOMContentLoaded', function (){
       };
       return liveNeighbor;
     });
-    console.log('zywiiii', liveNeighbor);
+    // console.log('zywiiii', liveNeighbor);
     if (this.position(x,y).classList.contains('live')&&liveNeighbor.length<2) {
-      console.log('op 1 zerooooo');
+      // console.log('op 1 zerooooo');
       return 0;
     } else if (this.position(x,y).classList.contains('live')&&liveNeighbor.length===2 || this.position(x,y).classList.contains('live')&&liveNeighbor.length===3) {
-      console.log('op2 jedeeeen');
+      // console.log('op2 jedeeeen');
         return 1;
       } else if (this.position(x,y).classList.contains('live')&&liveNeighbor.length>3) {
-        console.log('op3 zerooooo');
+        // console.log('op3 zerooooo');
         return 0;
     } else if ((!this.position(x,y).classList.contains('live'))&& liveNeighbor.length===3) {
       console.log('op4 jedeeeen');
       return 1;
+    } else {
+      // console.log('op5 zerooooo');
+      return 0;
     };
   };
 
-  // GameOfLife.prototype.computeNextGeneration = function() {
-  //   var nextGeneration = [];
-  //   for (var y = 0; y < this.height; y++) {
-  //     for (var x = 0; x < this.width; x++) {
-  //       nextGeneration.push(this.computeCellNextState(x,y));
-  //     }
-  //   };
-  //   console.log('next generationnnnn', nextGeneration);
-  //   return nextGeneration;
-  // }
+  GameOfLife.prototype.computeNextGeneration = function() {
+    var nextGeneration = [];
+    for (var y = 0; y < this.height; y++) {
+      for (var x = 0; x < this.width; x++) {
+        nextGeneration.push(this.computeCellNextState(x,y));
+        // console.log('next generationnnnn','x:', x, 'y:', y, nextGeneration);
+      }
+    };
+    console.log('next generationnnnn', nextGeneration);
+    return nextGeneration;
+  }
 
 
 
 
 
-  var game = new GameOfLife (20, 20, board);
+  var game = new GameOfLife (5,5, board);
   game.createBoard();
-  game.createNeighbors(0,0);
-  game.setCellState(5,5, 'live');
-  game.firstGlider(1,1, 'live');
-  game.computeCellNextState(0,0);
-  // game.computeNextGeneration();
+  // game.createNeighbors(0,0);
+  // game.setCellState(0,0, 'live');
+  game.firstGlider(3,3, 'live');
+  // game.computeCellNextState(3,3);
+  game.computeNextGeneration();
   console.log('position', game.position(1,1));
 
 
